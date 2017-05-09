7d78031c2b1278d0a1d1f85d0b3c6e2d734fd680
Jvaeyhcd <chedahuang@icloud.com>
2017-05-09 16:41:06 +0800

    first commit

---
+++ Example/HcdSpecialField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HcdSpecialField
+//
+//  Created by Jvaeyhcd on 09/05/2017.
+//  Copyright © 2017 Jvaeyhcd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
